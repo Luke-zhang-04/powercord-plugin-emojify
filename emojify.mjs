503d7165c5fc824f93e0a39056bdb90f81cb6f7f
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-04-24 16:54:38 -0400

    refactor: use regex instead of array of chars

--- emojify.mjs
+++ emojify.mjs
@@ -22,77 +22,10 @@ function* range(start, stop, step = 1) {
     return
 }
 
-const validChars = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "0",
-    "_",
-    "-",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-]
-
 const stripWord = (word) =>
     word
         .split("")
-        .filter((c) => validChars.includes(c))
+        .filter((char) => /[A-z]|-|_/u.test(char))
         .join("")
         .toLowerCase()
 
